6ee7ace5ebcef8981ec6c7ec8f66fcc2d9b2f136
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-06-05 00:22:25 +0530

    update code resolve error 10

--- frontend/src/user/pages/UserDashboard.jsx
+++ frontend/src/user/pages/UserDashboard.jsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../features/auth/authSlice";
-import { usePizzaDraft } from "../hooks/usePizzaDraft";
+
+import { usePizzaDraft } from "../../hooks/usePizzaDraft";
 
 function UserDashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+
   usePizzaDraft();
 
   useEffect(() => {
